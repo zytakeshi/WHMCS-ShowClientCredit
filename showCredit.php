3f799c3d6a27a51b64fec620ca4476e08321f197
zytakeshi <zyvista@gmail.com>
2024-09-24 19:20:12 +0900

    Update showCredit.php

--- showCredit.php
+++ showCredit.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * WHMCS - Client Credit Balance
+ * WHMCS - Client Credit Balance (Multilingual)
  * 
- * Adds a "Credit Balance" panel to the client area homepage.
+ * Adds a "Credit Balance" panel to the client area homepage with multilingual support.
  */
 
 use WHMCS\View\Menu\Item as Item;
@@ -21,13 +21,21 @@ add_hook('ClientAreaHomepagePanels', 1, function (Item $homePagePanels) {
         // Format the credit balance.
         $formattedCredit = formatCurrency((float)$client->credit, $currency);
         
-        // Add the "Credit Balance" panel to the homepage.
+        // Get translations for available credit and add funds button.
+        $availableCreditLabel = Lang::trans('availcreditbal'); // Available Credit Balance label
+        $availableCreditDesc = Lang::trans('availcreditbaldesc'); // Credit balance description
+        $addFundsText = Lang::trans('addfunds'); // Add Funds button text
+        
+        // Prepare the body HTML using translations.
+        $bodyHtml = '<p>' . sprintf($availableCreditDesc, $formattedCredit) . '</p>';
+        
+        // Add the "Credit Balance" panel to the homepage panels with translations.
         $homePagePanels->addChild('credit-balance', array(
-            'label' => 'Available Credit',
+            'label' => $availableCreditLabel,
             'icon'  => 'fa-money-bill-alt',
-            'order' => 100,
-            'bodyHtml' => '<p>Your available credit is: ' . $formattedCredit . '</p>',
-            'footerHtml' => '<a href="clientarea.php?action=addfunds" class="btn btn-primary">Add Funds</a>'
+            'order' => 1,  // Set to 1 to make it appear at the top
+            'bodyHtml' => $bodyHtml,
+            'footerHtml' => '<a href="clientarea.php?action=addfunds" class="btn btn-primary">' . $addFundsText . '</a>'
         ));
     }
 });
